dd4b01e3e8890a1b4b1c519bef75a2dce48c3942
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2024-04-22 13:28:59 +0530

    routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 // controllers
 import {
-  createUser,
-  loginUser,
-  logoutCurrentUser,
-  getAllUsers,
-  getCurrentUserProfile,
-  updateCurrentUserProfile,
+     createUser,
+     loginUser,
+     logoutCurrentUser,
+     getAllUsers,
+     getCurrentUserProfile,
+     updateCurrentUserProfile,
 } from "../controllers/userController.js";
 
 // middlewares
@@ -15,16 +15,16 @@ import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 router
-  .route("/")
-  .post(createUser)
-  .get(authenticate, authorizeAdmin, getAllUsers);
+     .route("/")
+     .post(createUser)
+     .get(authenticate, authorizeAdmin, getAllUsers);
 
 router.post("/auth", loginUser);
 router.post("/logout", logoutCurrentUser);
 
 router
-  .route("/profile")
-  .get(authenticate, getCurrentUserProfile)
-  .put(authenticate, updateCurrentUserProfile);
+     .route("/profile")
+     .get(authenticate, getCurrentUserProfile)
+     .put(authenticate, updateCurrentUserProfile);
 
 export default router;
