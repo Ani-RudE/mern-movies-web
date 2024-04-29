ac1520105180579e4508fe7ab774ba27f8e881c9
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2024-04-29 08:51:47 +0530

    api

--- frontend/src/redux/api/genre.js
+++ frontend/src/redux/api/genre.js
@@ -2,39 +2,39 @@ import { apiSlice } from "./apiSlice";
 import { GENRE_URL } from "../constants";
 
 export const genreApiSlice = apiSlice.injectEndpoints({
-  endpoints: (builder) => ({
-    createGenre: builder.mutation({
-      query: (newGenre) => ({
-        url: `${GENRE_URL}`,
-        method: "POST",
-        body: newGenre,
-      }),
-    }),
+     endpoints: (builder) => ({
+          createGenre: builder.mutation({
+               query: (newGenre) => ({
+                    url: `${GENRE_URL}`,
+                    method: "POST",
+                    body: newGenre,
+               }),
+          }),
 
-    updateGenre: builder.mutation({
-      query: ({ id, updateGenre }) => ({
-        url: `${GENRE_URL}/${id}`,
-        method: "PUT",
-        body: updateGenre,
-      }),
-    }),
+          updateGenre: builder.mutation({
+               query: ({ id, updateGenre }) => ({
+                    url: `${GENRE_URL}/${id}`,
+                    method: "PUT",
+                    body: updateGenre,
+               }),
+          }),
 
-    deleteGenre: builder.mutation({
-      query: (id) => ({
-        url: `${GENRE_URL}/${id}`,
-        method: "DELETE",
-      }),
-    }),
+          deleteGenre: builder.mutation({
+               query: (id) => ({
+                    url: `${GENRE_URL}/${id}`,
+                    method: "DELETE",
+               }),
+          }),
 
-    fetchGenres: builder.query({
-      query: () => `${GENRE_URL}/genres`,
-    }),
-  }),
+          fetchGenres: builder.query({
+               query: () => `${GENRE_URL}/genres`,
+          }),
+     }),
 });
 
 export const {
-  useCreateGenreMutation,
-  useUpdateGenreMutation,
-  useDeleteGenreMutation,
-  useFetchGenresQuery,
+     useCreateGenreMutation,
+     useUpdateGenreMutation,
+     useDeleteGenreMutation,
+     useFetchGenresQuery,
 } = genreApiSlice;
