2440719919ae97132ebfe7478ba37b433a603bf6
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2024-05-06 11:18:19 +0530

    console-msg

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log(`Successfully connected to MongoDB 👍`);
+    console.log(`Successfully connected to MongoDB! ✅`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
